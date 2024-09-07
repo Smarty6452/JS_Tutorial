47c580dc29f7a4e309f5047da56e441072269a46
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2024-09-07 19:11:12 -0400

    basic clear

---
+++ 01_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; // treal all code as newer version
+
+alert("hi")  //we are not using js browser
+
+console.log(3 + 3)
+
+
+let name = "hitesh"
+let age = 18;
+let isLoggedIn = true
+let state;
+let states = null;
+
+
+
+//number
+//bigInt
+//string
+// number 
+// string => ""
+// boolean = true/false
+// null 
+// undefined 
+// object 
+
+
+
